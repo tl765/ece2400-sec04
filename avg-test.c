4d47d8aecfe7f155665440cbb8170273908d3864
tl765 <tl765@cornell.edu>
2024-03-01 12:57:06 -0500

    really working on sec04

--- avg-test.c
+++ avg-test.c
@@ -1,5 +1,28 @@
+
+#include "ece2400-stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 int avg( int x, int y )
 {
   int sum = x + y;
-  return sum / 2;
+  return sum/2;
 }
+
+void test_case_1_basic()
+{
+  printf("\n%s\n", __func__  );
+  ECE2400_CHECK_INT_EQ( avg( 10, 20 ), 15 );
+}
+
+int main( int argc, char* argv[] )
+{
+  __n = ( argc == 1 ) ? 0 : atoi( argv[1] );
+
+  if ( (__n <= 0) || (__n == 1) )
+    test_case_1_basic();
+
+  printf( "\n" );
+  return __failed;
+}
+
